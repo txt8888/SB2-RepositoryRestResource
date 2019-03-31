e52924af7dd4693c3ef00909209f32c4151b2360
T. Phan <tphan88@yahoo.com>
2019-03-31 11:00:16 -0500

    added debug print of the departments in the /hello endpoint

--- src/main/java/org/phan/hr/controller/HelloWorld.java
+++ src/main/java/org/phan/hr/controller/HelloWorld.java
@@ -1,8 +1,13 @@
 package org.phan.hr.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.phan.hr.model.Department;
+import org.phan.hr.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by tcp on 03/31/2019.
@@ -11,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class HelloWorld {
 
+    @Autowired
+    DepartmentRepository repository;
+
     @RequestMapping("/hello")
     public String index() {
         HelloWorld.log.trace("A TRACE Message");
@@ -18,6 +26,11 @@ public class HelloWorld {
         HelloWorld.log.info("An INFO Message");
         HelloWorld.log.warn("A WARN Message");
         HelloWorld.log.error("An ERROR Message");
+
+        List<Department> departments = this.repository.findAll();
+        for (Department department : departments) {
+            HelloWorld.log.info("employee: " + department.getName());
+        }
 
         return "Howdy! Check out the Logs to see the output...";
     }
--- src/main/java/org/phan/hr/model/Department.java
+++ src/main/java/org/phan/hr/model/Department.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -19,7 +20,10 @@ import javax.persistence.Table;
 public class Department {
 
     @Id
+    @Column(name = "dept_no")
     private String id;
+
+    @Column(name = "dept_name")
     private String name;
 }
 
